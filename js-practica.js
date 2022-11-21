dee0785936b0db6c5c1b29dcd6778a9804019087
rucev <ruedaflors@gmail.com>
2022-11-21 18:59:56 +0100

    Add Options List and Refactor Code

--- js-practica.js
+++ js-practica.js
@@ -1,11 +1,6 @@
 /* 
-  si utilizáis algún import en vuestra solución, recordad que hay que indicarle a node 
-  que estamos utilizando módulos. Para ello, debemos incluir el fichero package.json que 
-  veis en este repositorio. En caso de que no os funcione, contactadme por discord.
+Código base del challenge
 */
-
-const { get } = require('http');
-const { exit } = require('process');
 
 const students = [{
   age: 32,
@@ -24,11 +19,9 @@ const availableMaleNames = ['pepe', 'juan', 'victor', 'leo', 'francisco', 'carlo
 const availableFemaleNames = ['cecilia', 'ana', 'luisa', 'silvia', 'isabel', 'virginia'];
 const availableGenders = ['male', 'female'];
 
-const optionsAvailable = [
-  () => { exit },
-  () => { console.table(students) }
-]
-
+/* 
+Funciones y tal
+*/
 
 const readline = require('readline').createInterface({
   input: process.stdin,
@@ -37,19 +30,12 @@ const readline = require('readline').createInterface({
 
 sum = (array) => {
   return array.reduce((valueA, valueB) => valueA + valueB, 0)
-}
+};
 
 meanAverage =(array) => {
-  return sum(array) / array.length};
+  return +Math.round(((sum(array) / array.length) + Number.EPSILON) * 100) / 100};
 
-/*1 Mostrar en formato de tabla todos los alumnos.*/
-optionsAvailable[1]();
-
-/*2 Mostrar por consola la cantidad de alumnos que hay en clase.*/
-console.log(students.length);
-
-/*3 Mostrar por consola todos los nombres de los alumnos.*/
-getNames = (studentsArray) => {
+const getNames = (studentsArray) => {
   let names = []
   for (let index = 0; index < studentsArray.length; index++) {
     names.push(studentsArray[index].name)
@@ -57,20 +43,17 @@ getNames = (studentsArray) => {
   console.log(names.join())
 };
 
-/*4 Eliminar el último alumno de la clase.*/
-deleteLast = (studentsArray) => {
+const deleteLast = (studentsArray) => {
   studentsArray.length = studentsArray.length - 1;
   return studentsArray
 };
 
-/*5 Eliminar un alumno aleatoriamente de la clase.*/
-deleteRandom = (studentsArray) => {
+const deleteRandom = (studentsArray) => {
   let student = (Math.random() * studentsArray.length) | 0
   return studentsArray.splice(student, 1)
 };
 
-/*6 Mostrar por consola todos los datos de los alumnos que son chicas.*/
-getFemalesList = (studentsArray) => {
+const getFemalesList = (studentsArray) => {
   let females= [];
   for (let index = 0; index < studentsArray.length; index++) {
     if (studentsArray[index].gender === 'female'){
@@ -80,38 +63,22 @@ getFemalesList = (studentsArray) => {
   return females
 };
 
-console.log(getFemalesList(students))
-
-/*7 Mostrar por consola el número de chicos y chicas que hay en la clase.*/
-console.log('female students:' + getFemalesList(students).length + '\n' + 'male students:'+ (students.length-getFemalesList(students).length));
-
-/*8 Mostrar true o false por consola si todos los alumnos de la clase son chicas.*/
-
-checkAllStudentsAreFemale = (studentsArray) => {
+const checkAllStudentsAreFemale = (studentsArray) => {
   let isTrue = studentsArray.every(student => student.gender === 'female');
   return console.log(isTrue)
-}
+};
 
-/*9 Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años.*/
-getYoungStudents = (studentsArray) => {
+const getYoungStudents = (studentsArray) => {
   let youngStudents = [];
   for (let index = 0; index < studentsArray.length; index++) {
     if ((studentsArray[index].age >= 20) && (studentsArray[index].age <= 25)){
       youngStudents.push(studentsArray[index].name);
     }
   };
-  return console.log(youngStudents)
+  return console.log(youngStudents.join())
 };
 
-/*10 Añadir un alumno nuevo con los siguientes datos:
-  - nombre aleatorio.
-  - edad aleatoria entre 20 y 50 años.
-  - género aleatorio.
-  - listado de calificaciones vacío.
-
-¡OJO!, el nombre y el género tienen que ir acordes.*/
-
-addRandomStudent = (studentsArray) => {
+const addRandomStudent = (studentsArray) => {
   let studentAge = Math.floor(Math.random() * 30) + 20;
   let studentGender = availableGenders[Math.floor(Math.random()*availableGenders.length)];
   let studentName = '';
@@ -127,12 +94,9 @@ addRandomStudent = (studentsArray) => {
     name: studentName
   };
   return studentsArray.push(newStudent);
-  };
+};
 
-/*11 Mostrar por consola el nombre de la persona más joven de la clase.
-¡OJO!, si varias personas de la clase comparten la edad más baja, cualquiera de ellos es una respuesta válida. */
-
-getYoungestStudent = (studentsArray) => {
+const getYoungestStudent = (studentsArray) => {
   let minAge = Math.min(...studentsArray.map(student => student.age));
   let minAgeData = studentsArray.filter(student => student.age === minAge);
   youngestStudentsNames = []
@@ -140,59 +104,48 @@ getYoungestStudent = (studentsArray) => {
     youngestStudentsNames.push(minAgeData[index].name)
   };
   return console.log(youngestStudentsNames.join())
-}
+};
 
-/*12 Mostrar por consola la edad media de todos los alumnos de la clase.*/
-getAgeAverage = (studentsArray) => {
+const getAgeAverage = (studentsArray) => {
   studentsAge = []
   for (let index = 0; index < studentsArray.length; index++) {
     studentsAge.push(studentsArray[index].age)
   };
   return console.log(meanAverage(studentsAge))
-}
+};
 
-/*13 Mostrar por consola la edad media de las chicas de la clase.*/
-getAgeAverage(getFemalesList(students));
-
-/*14 Añadir nueva nota a los alumnos. Por cada alumno de la clase, tendremos que calcular una nota de forma aleatoria(número entre 0 y 10) y añadirla a su listado de notas.*/
-addGrades = (studentsArray) => {
+const addGrades = (studentsArray) => {
   for (let index = 0; index < studentsArray.length; index++) {
     studentsArray[index].examScores.push(Math.floor(Math.random() * 11))
   };
   return studentsArray
 };
 
-/*15 Ordenar el array de alumnos alfabéticamente según su nombre. */
-students.sort((itemA, itemB) => itemA.name.toLowerCase().localeCompare(itemB.name.toLowerCase()));
+const sumGrades = (student) => {
+  return sum(student.examScores)
+};
 
-/*16 Mostrar por consola el alumno de la clase con las mejores notas.
-El alumno con mejores notas es aquel cuyo sumatorio de todas sus notas es el valor más alto de todos.*/
-getBestStudents = (studentsArray) => {
-  let bestStudent = [];
-  for (let index = 0; index < studentsArray.length; index++) {
-    if (bestStudent.length === 0) {
-      bestStudent.push(studentsArray[index])
-    } else if (sum(studentsArray[index].examScores) > sum(bestStudent[0].examScores)){
-      bestStudent[0] = studentsArray[index]
-    } else if (sum(studentsArray[index].examScores) === sum(bestStudent[0].examScores)){
+const getBestStudents = (studentsArray) => {
+  let bestStudent = [studentsArray[0]];
+  let bestStudentGrades = sumGrades(bestStudent[0])
+  for (let index = 1; index < studentsArray.length; index++) {
+    if (sumGrades(studentsArray[index]) > bestStudentGrades){
+      bestStudent = [studentsArray[index]]
+      bestStudentGrades = sumGrades(studentsArray[index])
+    } else if (sumGrades(studentsArray[index]) === bestStudentGrades){
       bestStudent.push(studentsArray[index])
     };
-  }
+  };
   return bestStudent
-}
+};
 
-getNames(getBestStudents(students))
-
-/*17 Mostrar por consola la nota media más alta de la clase y el nombre del alumno al que pertenece.*/
-getStudentsNamesAndGradeAverage = (studentsArray) => {
+const getStudentsNamesAndGradeAverage = (studentsArray) => {
   for (let index = 0; index < studentsArray.length; index++) {
-    console.log(studentsArray[index].name+ ' ' +Math.round(((meanAverage(studentsArray[index].examScores)) + Number.EPSILON) * 100) / 100)
+    console.log(studentsArray[index].name+ ' ' +meanAverage(studentsArray[index].examScores))
   }
-}
+};
 
-/*### 18- Añadir un punto extra a cada nota existente de todos los alumnos. Recordad que la nota máxima posible es 10. Si los alumnos aún no tienen registrada ninguna nota, les pondremos un 10.
-*/
-addGradePoint = (grades) => {
+const addGradePoint = (grades) => {
   if  (grades.length === 0) {
     grades.push(10)
   } else { for (let index = 0; index < grades.length; index++) {
@@ -203,8 +156,109 @@ addGradePoint = (grades) => {
   return grades
 }
 
-addPointsToAll = (studentsArray) => {
+const addPointsToAll = (studentsArray) => {
   for (let index = 0; index < studentsArray.length; index++) {
     addGradePoint(studentsArray[index].examScores)
   }
 }
+
+/* 
+Menu de opciones
+*/
+
+const optionsAvailable = [{
+  id: 0,
+  description: 'exit',
+  function: () => exit()
+  },
+  {
+  id: 1,
+  description: 'Mostrar en formato de tabla todo el alumnado.',
+  function: () => console.table(students)
+},
+{
+  id: 2,
+  description: 'Mostrar por consola la cantidad de alumnado que hay en clase.',
+  function: () => console.log('\n'+'Cantidad estudiantes:'+'\n'+students.length)
+},
+{
+  id: 3,
+  description: 'Mostrar por consola todos los nombres del alumnado.',
+  function: () => {console.log('\n'+'Nombres alumnado:'); getNames(students)}
+},
+{
+  id: 4,
+  description: 'Eliminar el último alumno/a de la clase.',
+  function: () => deleteLast(students)
+},
+{
+  id: 5,
+  description: 'Eliminar un alumno/a aleatoriamente de la clase.',
+  function: () => deleteRandom(students)
+},
+{
+  id: 6,
+  description: 'Mostrar por consola todos los datos del alumnado femenino.',
+  function: () => {console.log('\n'+'Datos alumnas:'); console.log(getFemalesList(students))}
+},
+{
+  id: 7,
+  description: 'Mostrar por consola el número de chicos y chicas que hay en la clase.',
+  function: () => console.log('\n'+'Cantidad alumnas: ' + getFemalesList(students).length + '\n' + 'Cantidad alumnos: '+ (students.length-getFemalesList(students).length))
+},
+{
+  id: 8,
+  description: 'Mostrar true o false por consola si todo el alumnado de la clase son chicas.',
+  function: () => {console.log('¿Alumnado 100% femenino?'); checkAllStudentsAreFemale(students)}
+},
+{
+  id: 9,
+  description: 'Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años.',
+  function: () => {console.log('\n'+'Alumnado de 20-25 años:'); getYoungStudents(students)}
+},
+{
+  id: 10,
+  description: 'Añadir estudiante random.',
+  function: () => addRandomStudent(students)
+},
+{
+  id: 11,
+  description: 'Mostrar por consola el nombre de la persona más joven de la clase.',
+  function: () => {console.log('\n'+'Estudiante más joven:'); getYoungestStudent(students)}
+},
+{
+  id: 12,
+  description: 'Mostrar por consola la edad media de todo el alumnado de la clase.',
+  function: () => {console.log('\n'+'Edad media del alumnado:'); getAgeAverage(students)}
+},
+{
+  id: 13,
+  description: 'Mostrar por consola la edad media de las chicas de la clase.',
+  function: () => {console.log('\n'+'Edad media alumnas:'); getAgeAverage(getFemalesList(students))}
+},
+{
+  id: 14,
+  description: 'Añadir nueva nota (random) a todo el alumnado',
+  function: () => addGrades(students)
+},
+{
+  id: 15,
+  description: 'Ordenar al alumnado alfabéticamente según su nombre.',
+  function: () => students.sort((itemA, itemB) => itemA.name.toLowerCase().localeCompare(itemB.name.toLowerCase()))
+},
+{
+  id: 16,
+  description: 'Mostrar por consola el nombre del/la estudiante con las mejores notas.',
+  function: () => {console.log('\n'+'Estudiante/s con mejores notas:'); getNames(getBestStudents(students))}
+},
+{
+  id: 17,
+  description: 'Mostrar por consola la nota media más alta de la clase y a qué estudiante pertenece.',
+  function: () => {console.log('\n'+'Estudiante/s con mejores notas y su nota media:'); getStudentsNamesAndGradeAverage(getBestStudents(students))}
+},
+{
+  id: 18,
+  description: 'Añadir un punto extra a cada nota existente de todo el alumnado. Si no tienen registrada ninguna nota, se les pone un 10.',
+  function: () => addPointsToAll(students)
+}
+]
